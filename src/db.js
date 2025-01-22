b7475b784031e9be4200f313393779a0851be44f
gustavogss <gustavogss.dev@gmail.com>
2025-01-21 23:14:58 -0300

    feat: finished project

--- src/db.js
+++ src/db.js
@@ -1,34 +1,59 @@
-import fs from 'node:fs/promises'
+import fs from "node:fs/promises";
 
-const dbPath = new URL('../db.json', import.meta.url)
+const dbPath = new URL("../db.json", import.meta.url);
 
 export class DB {
-  #db = { }
+  #db = {};
 
-  constructor(){
-    fs.readFile(dbPath, 'utf-8').then(data =>{
-      this.#db = JSON.parse(data)
-    }).catch(()=>{
-      this.#persist()
-    })
+  constructor() {
+    fs.readFile(dbPath, "utf-8")
+      .then((data) => {
+        this.#db = JSON.parse(data);
+      })
+      .catch(() => {
+        this.#persist();
+      });
   }
 
   #persist() {
-    fs.writeFile(dbPath, JSON.stringify(this.#db))
+    fs.writeFile(dbPath, JSON.stringify(this.#db));
   }
 
-  select(table){
-    const data = this.#db[table] ?? []
-    return data
+  select(table, search) {
+    let data = this.#db[table] ?? [];
+    if (search) {
+      data = data.filter((row) => {
+        return Object.entries(search).some(([key, value]) => {
+          return row[key].toLowerCase().includes(value.toLowerCase());
+        });
+      });
+    }
+    return data;
   }
 
-  insert(table, data){
-      if (Array.isArray(this.#db[table])){
-        this.#db[table].push(data)
-      } else {
-        this.#db[table] = [data]
-      }
+  insert(table, data) {
+    if (Array.isArray(this.#db[table])) {
+      this.#db[table].push(data);
+    } else {
+      this.#db[table] = [data];
+    }
+    this.#persist();
+    return data;
+  }
+
+  delete(table, id) {
+    const rowIndex = this.#db[table].findIndex((row) => row.id === id);
+    if (rowIndex > -1) {
+      this.#db[table].splice(rowIndex, 1);
       this.#persist();
-      return data;
+    }
+  }
+
+  update(table, id, data) {
+    const rowIndex = this.#db[table].findIndex((row) => row.id === id);
+    if (rowIndex > -1) {
+      this.#db[table][rowIndex] = { id, ...data };
+      this.#persist();
+    }
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,30 @@
 import { DB } from "./db.js";
 import { randomUUID } from "node:crypto";
+import { buildRoutePath } from "./utils/build-route-path.js";
 
 const db = new DB();
 
 export const routes = [
   {
     method: "GET",
-    path: "/users",
+    path: buildRoutePath("/users"),
     handler: (req, res) => {
-      const users = db.select("users");
+      const { search } = req.query;
+      const users = db.select(
+        "users",
+        search
+          ? {
+              name: search,
+              email: search,
+            }
+          : null
+      );
       return res.end(JSON.stringify(users));
     },
   },
   {
     method: "POST",
-    path: "/users",
+    path: buildRoutePath("/users"),
     handler: (req, res) => {
       const { name, email } = req.body;
 
@@ -25,6 +35,31 @@ export const routes = [
       };
       db.insert("users", user);
       return res.writeHead(201).end("Usuário Criado");
+    },
+  },
+  {
+    method: "DELETE",
+    path: buildRoutePath("/users/:id"),
+    handler: (req, res) => {
+      const { id } = req.params;
+
+      db.delete("users", id);
+
+      return res.writeHead(204).end();
+    },
+  },
+  {
+    method: "PUT",
+    path: buildRoutePath("/users/:id"),
+    handler: (req, res) => {
+      const { id } = req.params;
+      const { name, email } = req.body;
+
+      db.update("users", id, {
+        name,
+        email,
+      });
+      return res.writeHead(204).end();
     },
   },
 ];
